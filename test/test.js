abb65084a834f6dba8fb254f638564ad00d268d0
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-06-11 13:22:53 +0100

    manual testing of req-res

--- test/test.js
+++ test/test.js
@@ -1,17 +1,26 @@
 var client = require('../webshot-queue-client');
 
-client.imageExists('47e7f3cf-5ef8-4706-84e0-d6fe5184ebee', function(res) {
-	console.log('should exist ' + res);
-});
+client.init(function() {
+	client.imageExists('47e7f3cf-5ef8-4706-84e0-d6fe5184ebee', function(res) {
+		console.log('should exist ' + res);
+	});
 
-client.imageExists('47e7f3cf-5ef8-4706-84e0-d6fe5184ebee', 'png', function(res) {
-	console.log('should exist ' + res);
-});
+	client.imageExists('47e7f3cf-5ef8-4706-84e0-d6fe5184ebee', 'png', function(res) {
+		console.log('should exist ' + res);
+	});
 
-client.imageExists('47e7f3cf-5ef8-4706-84e0-DOESNOTEXIST', function(res) {
-	console.log('should NOT exist ' + res);
-});
+	client.imageExists('47e7f3cf-5ef8-4706-84e0-DOESNOTEXIST', function(res) {
+		console.log('should NOT exist ' + res);
+	});
 
-client.imageExists('47e7f3cf-5ef8-4706-84e0-DOESNOTEXIST', 'png', function(res) {
-	console.log('should NOT exist ' + res);
+	client.imageExists('47e7f3cf-5ef8-4706-84e0-DOESNOTEXIST', 'png', function(res) {
+		console.log('should NOT exist ' + res);
+	});
+
+
+	client.onScreenshot(function(err, msg) {
+		if (err) { console.log(err); }
+		console.log('got screenshot: ' + JSON.stringify(msg));
+	})
+	client.requestScreenshot('http://www.google.com');
 });
