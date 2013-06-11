6e14fa864dc484441a51010dadf167e776ad399d
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-06-11 10:58:36 +0100

    modules, config

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	amqp: {
+		credentials: {
+			host: "localhost",
+			port: 5672,
+			username: "guest",
+			password: "guest",
+			vhost: "/"
+		},
+		requestProducer: {
+			exchangeName: "screenshot.exchange",
+			queueName: "screenshot.request",
+			routingKey: "request"
+		},
+		responseConsumer: {
+			exchangeName: "screenshot.exchange",
+			queueName: "screenshot.response",
+			routingKey: "response"
+		}
+	}
+};
